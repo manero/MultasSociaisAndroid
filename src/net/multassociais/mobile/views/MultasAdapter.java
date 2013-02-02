bd4ef5f7be9f65100237ff9da390208afcc4d948
Rafael Duarte Lucchesi <raflucchesi@gmail.com>
2013-02-02 02:50:00 -0200

    Refatorado o adapter de multas para usar a view final

--- src/net/multassociais/mobile/views/MultasAdapter.java
+++ src/net/multassociais/mobile/views/MultasAdapter.java
@@ -1,5 +1,6 @@
 package net.multassociais.mobile.views;
 
+import net.multassociais.mobile.R;
 import net.multassociais.mobile.modelos.Multa;
 import net.multassociais.mobile.modelos.Multa.MultasCollection;
 import android.content.Context;
@@ -7,27 +8,39 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.androidquery.AQuery;
 
 public class MultasAdapter extends ArrayAdapter<Multa> {
     
+    private AQuery mAquery;
     private LayoutInflater mInflater;
 
     public MultasAdapter(Context context, MultasCollection objects) {
-        super(context, android.R.layout.simple_list_item_1, objects);
+        super(context, R.layout.multa_item_view, objects);
+        
         mInflater = LayoutInflater.from(getContext());
+        mAquery = new AQuery(context);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        TextView rowView = (TextView) convertView;
-        if (rowView == null) {
-            rowView = (TextView) mInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.multa_item_view, parent, false);
         }
         
-        rowView.setText(getItem(position).getTitle());
+        Multa multa = getItem(position);
         
-        return rowView;
+        TextView descView = (TextView) convertView.findViewById(R.id.descricao);
+        descView.setText(multa.getTitle());
+        
+        ImageView imageView = (ImageView) convertView.findViewById(R.id.thumb);
+        mAquery.id(imageView).image(multa.getFotoPequena());
+//        imageView.setImageURI(Uri.parse(multa.getFotoPequena()));
+        
+        return convertView;
     }
     
 }
